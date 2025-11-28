9237d612f2d38fb187d3b21ca88995f710ce9706
pranshu9179 <0112ca221094@gmail.com>
2025-11-28 15:14:06 +0530

    new updated code

--- src/layout/SidebarLayout.jsx
+++ src/layout/SidebarLayout.jsx
@@ -200,7 +200,7 @@ const SideBarLayout = ({ children }) => {
   const links = [
     {
       label: "Dashboard",
-      href: "/",
+      href: "/dashboard",
       icon: <IconBrandTabler className="h-5 w-5" />,
     },
     {
@@ -210,7 +210,7 @@ const SideBarLayout = ({ children }) => {
     },
     {
       label: "Student Form",
-      href: "/register",
+      href: "/",
       icon: <IconUserBolt className="h-5 w-5" />,
     },
     // {
